9c8740a4eee3afec736fc40ec1e36c6cf310bed9
coaa0801-creator <68447827+coaa0801-creator@users.noreply.github.com>
2020-08-28 16:20:28 -0600

    Total and Payment made

--- src/makeChange/makeChange.java
+++ src/makeChange/makeChange.java
@@ -1,10 +1,27 @@
 package makeChange;
 
+import java.util.Scanner;
+
 public class makeChange {
 	
+	public static double Total() {
+		Scanner kb = new Scanner(System.in);
+		System.out.print("Thank you for coming in!\n\nWhat is the total on the items you have there?  ");
+		double total = kb.nextDouble();
+		return total;
+	}
+	public static double Payment() {
+		Scanner kb = new Scanner(System.in);
+	System.out.print("Whoa! Well what amount were you thinking of paying?  ");
+	double payment = kb.nextDouble();
+	return payment;
+	}
+		
+	
 	public static void main(String[] args) {
-		
-		
+		Scanner kb = new Scanner(System.in);
+		Total();
+		Payment();
 		
 		
 		
