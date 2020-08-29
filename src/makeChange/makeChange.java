145ee0f93d3f085fcb0f299553b0760ce8309709
coaa0801-creator <68447827+coaa0801-creator@users.noreply.github.com>
2020-08-28 20:35:14 -0600

    Everything Except how to account for results equaling an exact denom

--- src/makeChange/makeChange.java
+++ src/makeChange/makeChange.java
@@ -5,98 +5,6 @@ import java.util.Scanner;
 
 public class makeChange {
 
-	public static String Currency(double change) {
-		int quantity = 0;
-		if (change > 20) {
-		do      { 
-			change = change - 20;
-			quantity++;
-		} while ((change - 20) > 0);
-		System.out.print(quantity + " - $20 bills ");
-		}
-		if (change > 10) {
-			do      { 
-				change = change % 20;
-				quantity = 0;
-				change = change - 10;
-				quantity++;
-			} while ((change - 10) > 0);
-			System.out.print(quantity + " - $10 bills ");
-		}
-		if (change > 5) {
-			do      { 
-				change = change % 10;
-				quantity = 0;
-				change = change - 5;
-				quantity++;
-			} while ((change - 5) > 0);
-			System.out.print(quantity + " - $5 bills ");
-		}
-		if (change > 1) {
-			do      { 
-				change = change % 5;
-				quantity = 0;
-				change = change - 1;
-				quantity++;
-			} while ((change - 1) > 0);
-			System.out.print(quantity + " - $1 bills ");
-		}
-		
-		
-		
-		
-		
-		
-//		for (int quantity = 0; change > quantity; quantity++) {
-//			if ((change - 20) > 0) {
-//				change = change - 20;
-//			}
-						
-						
-			
-		
-		
-		
-		
-		
-		
-		
-		
-		
-
-		return "";
-
-	}
-
-	public static String GetChange(double amount, double pay) {
-		DecimalFormat rndDiff = new DecimalFormat("#.00");
-		double changeNeeded = pay - amount;
-		System.out.println("Looks like that will be $" + (rndDiff.format(changeNeeded)) + " coming back to you");
-		Currency(changeNeeded);
-
-		return "\n\nThank You!!";
-	}
-
-	public static double Total() {
-		Scanner kb = new Scanner(System.in);
-		System.out.print("Thank you for coming in!\n\nWhat is the total on the items you have there?  ");
-		double total = kb.nextDouble();
-		return total;
-	}
-
-	public static double Payment() {
-		Scanner kb = new Scanner(System.in);
-		System.out.print("Whoa! Well what amount were you thinking of paying?  ");
-		double payment = kb.nextDouble();
-		return payment;
-	}
-
-	public static double addMore() {
-		Scanner kb = new Scanner(System.in);
-		System.out.println("How much more would you like to add to your payment?");
-		double newPay = kb.nextDouble();
-		return newPay;
-	}
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
@@ -115,6 +23,146 @@ public class makeChange {
 		}
 		System.out.println("Thank you for your payment. Let me grab your change... \n");
 		System.out.println(GetChange(amount, pay));
-
+kb.close();
+	}
+	public static String Currency(double change) {
+		int quantity = 0;
+		if (change > 20) {
+			do {
+				change = change - 20;
+				quantity++;
+			} while ((change - 20) > 0);
+			if (quantity == 1) {
+				System.out.println(quantity + " - $20 bill ");
+			} else {
+				System.out.println(quantity + " - $20 bills ");
+			}
+		}
+		
+		if (change > 10) {
+			do {
+				change = change % 20;
+				quantity = 0;
+				change = change - 10;
+				quantity++;
+			} while ((change - 10) > 0);
+			if (quantity == 1) {
+				System.out.println(quantity + " - $10 bill ");
+			} else {
+				System.out.println(quantity + " - $10 bills ");
+			}
+		}
+		if (change > 5) {
+			do {
+				change = change % 10;
+				quantity = 0;
+				change = change - 5;
+				quantity++;
+			} while ((change - 5) > 0);
+			if (quantity == 1) {
+				System.out.println(quantity + " - $5 bill ");
+			} else {
+				System.out.println(quantity + " - $5 bills ");
+			}
+		}
+		if (change > 1) {
+			do {
+				change = change % 5;
+				quantity = 0;
+				change = change - 1;
+				quantity++;
+			} while ((change - 1) > 0);
+			if (quantity == 1) {
+				System.out.println(quantity + " - $1 bill ");
+			} else {
+				System.out.println(quantity + " - $1 bills ");
+			}
+		}
+		if (change > 0.25) {
+			do {
+				change = change % 1;
+				quantity = 0;
+				change = change - 0.25;
+				quantity++;
+			} while ((change - 0.25) > 0);
+			if (quantity == 1) {
+				System.out.println(quantity + " - Quarter ");
+			} else {
+				System.out.println(quantity + " - Quarters ");
+			}
+		}
+		if (change > 0.10) {
+			do {
+				change = change % 0.25;
+				quantity = 0;
+				change = change - 0.10;
+				quantity++;
+			} while ((change - 0.10) > 0);
+			if (quantity == 1) {
+				System.out.println(quantity + " - Dime ");
+			} else {
+				System.out.println(quantity + " - Dimes ");
+			}
+		}
+		if (change > 0.05) {
+			do {
+				change = change % 0.10;
+				quantity = 0;
+				change = change - 0.05;
+				quantity++;
+			} while ((change - 0.05) > 0);
+			if (quantity == 1) {
+				System.out.println(quantity + " - Nickel ");
+			} else {
+				System.out.println(quantity + " - Nickels ");
+			}
+		}
+		if (change > 0.01) {
+			do {
+				change = change % 0.05;
+				quantity = 0;
+				change = change - 0.01;
+				quantity++;
+			} while ((change - 0.01) > 0);
+			if (quantity == 1) {
+				System.out.println(quantity + " - Penny ");
+			} else {
+				System.out.println(quantity + " - Pennies ");
+			}
+		}
+		
+		
+		return "";
+		
+	}
+	
+	public static String GetChange(double amount, double pay) {
+		DecimalFormat rndDiff = new DecimalFormat("#.00");
+		double changeNeeded = pay - amount;
+		System.out.println("Looks like that will be $" + (rndDiff.format(changeNeeded)) + " coming back to you\n\n");
+		Currency(changeNeeded);
+		
+		return "\n\nThank You!!";
+	}
+	
+	public static double Total() {
+		Scanner kb = new Scanner(System.in);
+		System.out.print("Thank you for coming in!\n\nWhat is the total on the items you have there?  ");
+		double total = kb.nextDouble();
+		return total;
+	}
+	
+	public static double Payment() {
+		Scanner kb = new Scanner(System.in);
+		System.out.print("Whoa! Well what amount were you thinking of paying?  ");
+		double payment = kb.nextDouble();
+		return payment;
+	}
+	
+	public static double addMore() {
+		Scanner kb = new Scanner(System.in);
+		System.out.println("How much more would you like to add to your payment?");
+		double newPay = kb.nextDouble();
+		return newPay;
 	}
 }
