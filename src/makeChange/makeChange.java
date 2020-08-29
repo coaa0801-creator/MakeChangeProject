b5b60b8d5b022b5171fa22a3a3963efe16da3868
coaa0801-creator <68447827+coaa0801-creator@users.noreply.github.com>
2020-08-28 22:26:18 -0600

    Good except pennies

--- src/makeChange/makeChange.java
+++ src/makeChange/makeChange.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class makeChange {
 
-
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 		double amount = Total();
@@ -15,39 +14,39 @@ public class makeChange {
 				System.out.print("Thank you for providing exact change! Have a nice day!");
 			} else if (amount > pay) {
 				DecimalFormat rndDiff = new DecimalFormat("#.00");
-				System.out.print("Well that's not enough, please provide at least $" + (rndDiff.format(amount - pay))
+				System.out.print("Well that's not enough... \n\nPlease provide at least $" + (rndDiff.format(amount - pay))
 						+ " more\n");
 				double newPay = addMore();
 				pay = pay + newPay;
 			}
 		}
-		System.out.println("Thank you for your payment. Let me grab your change... \n");
+		System.out.println("\nThank you for your payment. Let me grab your change... \n");
 		System.out.println(GetChange(amount, pay));
-kb.close();
+		kb.close();
 	}
+
 	public static String Currency(double change) {
 		int quantity = 0;
 		if (change >= 20) {
 			if ((change % 20) == 0) {
 				do {
+					change = change - 20;
+					quantity++;
+				} while ((change - 20) > 0);
+			}
+
+			while ((change - 20 > 0)) {
 				change = change - 20;
 				quantity++;
-			} while ((change - 20) > 0);
-				}
-			
-			do {
-				change = change - 20;
-				quantity++;
-			} while ((change - 20) > 0);
-			
+			}
+
 			if (quantity == 1) {
 				System.out.println(quantity + " - $20 bill ");
 			} else {
 				System.out.println(quantity + " - $20 bills ");
 			}
-			}
-		
-		
+		}
+
 		if (change >= 10) {
 			quantity = 0;
 			if ((change % 10) == 0) {
@@ -56,11 +55,11 @@ kb.close();
 					quantity++;
 				} while ((change - 10) > 0);
 			}
-			do {
+			while ((change - 10) > 0) {
 				change = change % 20;
 				change = change - 10;
 				quantity++;
-			} while ((change - 10) > 0);
+			}
 			if (quantity == 1) {
 				System.out.println(quantity + " - $10 bill ");
 			} else {
@@ -75,18 +74,18 @@ kb.close();
 					quantity++;
 				} while ((change - 5) > 0);
 			}
-			do {
+			while ((change - 5) > 0) {
 				change = change % 10;
 				change = change - 5;
 				quantity++;
-			} while ((change - 5) > 0);
+			}
 			if (quantity == 1) {
 				System.out.println(quantity + " - $5 bill ");
 			} else {
 				System.out.println(quantity + " - $5 bills ");
 			}
 		}
-		if (change >=1) {
+		if (change >= 1) {
 			quantity = 0;
 			if ((change % 1) == 0) {
 				do {
@@ -94,11 +93,11 @@ kb.close();
 					quantity++;
 				} while ((change - 1) > 0);
 			}
-			do {
+			while ((change - 1) > 0) {
 				change = change % 5;
 				change = change - 1;
 				quantity++;
-			} while ((change - 1) > 0);
+			}
 			if (quantity == 1) {
 				System.out.println(quantity + " - $1 bill ");
 			} else {
@@ -113,11 +112,11 @@ kb.close();
 					quantity++;
 				} while ((change - .25) > 0);
 			}
-			do {
+			while ((change - 0.25) > 0) {
 				change = change % 1;
 				change = change - 0.25;
 				quantity++;
-			} while ((change - 0.25) > 0);
+			}
 			if (quantity == 1) {
 				System.out.println(quantity + " - Quarter ");
 			} else {
@@ -132,11 +131,11 @@ kb.close();
 					quantity++;
 				} while ((change - 0.10) > 0);
 			}
-			do {
+			while ((change - .10) > 0) {
 				change = change % .25;
 				change = change - .10;
 				quantity++;
-			} while ((change - .10) > 0);
+			}
 			if (quantity == 1) {
 				System.out.println(quantity + " - Dime ");
 			} else {
@@ -151,68 +150,78 @@ kb.close();
 					quantity++;
 				} while ((change - .05) > 0);
 			}
-			do {
+			while ((change - .05) > 0) {
 				change = change % .10;
 				change = change - .05;
 				quantity++;
-			} while ((change - .05) > 0);
+			}
 			if (quantity == 1) {
 				System.out.println(quantity + " - Nickel ");
 			} else {
 				System.out.println(quantity + " - Nickels ");
 			}
 		}
-		if (change >= -.01) {
+		if (change >= .01) {
 			quantity = 0;
 			if ((change % .01) == 0) {
 				do {
 					change = change - .01;
 					quantity++;
-				} while ((change - .01) > 0);
+				} while ((change - .01) > -.01);
 			}
-			do {
+			while ((change - .01) > -.01) {
 				change = change % .05;
 				change = change - .01;
 				quantity++;
-			} while ((change - .01) > -.01);
+			}
 			if (quantity == 1) {
 				System.out.println(quantity + " - Penny ");
 			} else {
 				System.out.println(quantity + " - Pennies ");
 			}
 		}
-		
-		
+
 		return "";
-		
+
 	}
-	
+
 	public static String GetChange(double amount, double pay) {
 		DecimalFormat rndDiff = new DecimalFormat("#.00");
 		double changeNeeded = pay - amount;
 		System.out.println("Looks like that will be $" + (rndDiff.format(changeNeeded)) + " coming back to you\n\n");
 		Currency(changeNeeded);
-		
+
 		return "\n\nThank You!!";
 	}
-	
+
 	public static double Total() {
 		Scanner kb = new Scanner(System.in);
-		System.out.print("Thank you for coming in!\n\nWhat is the total on the items you have there?  ");
+		System.out.print("Thank you for coming in!\n\nWhat is the total on the items you have there?\t");
 		double total = kb.nextDouble();
+		if (total < 0) {
+			System.out.print(
+					"\nI'm sorry, this isn't the counter for returns. \n\nIf you'd like to purchase something, could you please give me the total?\t");
+			total = kb.nextDouble();
+		
+		}
 		return total;
 	}
-	
+
 	public static double Payment() {
 		Scanner kb = new Scanner(System.in);
-		System.out.print("Whoa! Well what amount were you thinking of paying?  ");
+		System.out.print("\nWhoa! What amount were you thinking of paying?\t");
 		double payment = kb.nextDouble();
+		if (payment < 0) {
+			System.out.print(
+					"\nI'm sorry, I can't pay you to take it no matter how crappy you think the quality is. \n\nYou must provide at least the total amount. What amount would you be paying?\t");
+			payment = kb.nextDouble();
+		}
 		return payment;
 	}
-	
+
 	public static double addMore() {
 		Scanner kb = new Scanner(System.in);
-		System.out.println("How much more would you like to add to your payment?");
+		System.out.print("\nHow much more would you like to add to your payment?\t");
 		double newPay = kb.nextDouble();
 		return newPay;
 	}
